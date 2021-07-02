065c4ca849b5e536672bcfe8741771cb0a42b46c
Alex Grounds <agrounds90@gmail.com>
2021-07-02 15:04:12 -0500

    Implement 2020 day5

---
+++ src/main/kotlin/advent/y2020/Day5.kt
@@ -0,0 +1,30 @@
+package advent.y2020
+
+import java.lang.RuntimeException
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+private fun String.fromPlaneBinary(): Int = this.map { c ->
+        when (c) {
+            'F' -> 0
+            'B' -> 1
+            'L' -> 0
+            'R' -> 1
+            else -> throw RuntimeException("Illegal char: $c")
+        }
+    }.joinToString("").toInt(2)
+
+fun main() {
+    val seatNumbers = (DATAPATH / "day5.txt").useLines { it.toList() }
+        .map { it.fromPlaneBinary() }
+
+    val max = seatNumbers.maxOrNull()
+        ?.also { println("Part one: $it") }
+        ?: throw RuntimeException("Empty list??")
+
+    seatNumbers.toSet().let { set ->
+        (max downTo 0).first { n ->
+            (n !in set)
+        }
+    }.also { println("Part two: $it") }
+}
