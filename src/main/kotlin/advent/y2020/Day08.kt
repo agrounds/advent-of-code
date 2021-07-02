229a0fb3017f73ee54c611491cba79255f84a0f7
Alex Grounds <agrounds90@gmail.com>
2021-07-02 16:53:13 -0500

    Implement 2020 day8

---
+++ src/main/kotlin/advent/y2020/Day08.kt
@@ -0,0 +1,75 @@
+package advent.y2020
+
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+private data class Instruction(val op: Op, val arg: Int)
+
+private enum class Op {
+    ACC, JMP, NOP
+}
+
+private data class ProgramResult(val accumulator: Int, val isLoop: Boolean)
+
+private fun executeProgram(program: List<Instruction>): ProgramResult {
+    var accumulator = 0
+    var next = 0
+    val prevInstructions = mutableSetOf<Int>()
+
+    while (next !in prevInstructions && next < program.size) {
+        prevInstructions.add(next)
+
+        val instruction = program[next]
+        when (instruction.op) {
+            Op.ACC -> {
+                accumulator += instruction.arg
+                next++
+            }
+            Op.JMP -> {
+                next += instruction.arg
+            }
+            Op.NOP -> {
+                // no op
+                next++
+            }
+        }
+    }
+
+    return ProgramResult(accumulator, next in prevInstructions)
+}
+
+fun main() {
+    val program = (DATAPATH / "day08.txt").useLines { lines ->
+        lines.map { line ->
+            val tokens = line.split(" ")
+            Instruction(Op.valueOf(tokens[0].uppercase()), tokens[1].toInt())
+        }.toMutableList()
+    }
+
+    executeProgram(program)
+        .also { println("Part one: $it") }
+
+    for (i in program.indices) {
+        val instruction = program[i]
+        when (instruction.op) {
+            Op.JMP -> {
+                program[i] = instruction.copy(op = Op.NOP)
+                val result = executeProgram(program)
+                if (!result.isLoop) {
+                    println("Part two: $result")
+                    break
+                }
+                program[i] = instruction
+            }
+            Op.NOP -> {
+                program[i] = instruction.copy(op = Op.JMP)
+                val result = executeProgram(program)
+                if (!result.isLoop) {
+                    println("Part two: $result")
+                    break
+                }
+                program[i] = instruction
+            }
+        }
+    }
+}
