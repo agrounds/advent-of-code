46db44472f0b7ceb0cd4210a228243ae0bbc4c8c
Alex Grounds <agrounds90@gmail.com>
2021-07-05 23:35:31 -0500

    Implement 2020 day15

---
+++ src/main/kotlin/advent/y2020/d15/Day15.kt
@@ -0,0 +1,33 @@
+package advent.y2020.d15
+
+private fun memoryGame(starting: List<Int>, query: Int): Int {
+    if (query < starting.size) return starting[query]
+
+    val lastSpoken = mutableMapOf<Int, Int>().apply {
+        starting.forEachIndexed { i, n ->
+            if (i != starting.size - 1) {
+                this[n] = i + 1
+            }
+        }
+    }
+    var prevNum = starting.last()
+
+    for (round in (starting.size + 1)..query) {
+        val thisNum = lastSpoken[prevNum]
+            ?.let { round - 1 - it }
+            ?: 0
+        lastSpoken[prevNum] = round - 1
+        prevNum = thisNum
+    }
+
+    return prevNum
+}
+
+fun main() {
+    val testInput = listOf(0, 3, 6)
+    val input = listOf(14, 8, 16, 0, 1, 17)
+
+    memoryGame(testInput, 2020).also { println("Test input: $it (should be 436)") }
+    memoryGame(input, 2020).also { println("Part one: $it") }
+    memoryGame(input, 30_000_000).also { println("Part two: $it") }
+}
