5a461e87bba6eeaf99b856f19c738d152fcd91fc
Alex Grounds <agrounds90@gmail.com>
2021-08-02 16:28:23 -0500

    Implement 2020 day17 part two

--- src/main/kotlin/advent/y2020/d17/Day17.kt
+++ src/main/kotlin/advent/y2020/d17/Day17.kt
@@ -48,21 +48,74 @@ private fun State.next(): State {
     return nextState
 }
 
-fun main() {
-    val initialState: State = (DATAPATH / "day17.txt").useLines { lines ->
-        lines.flatMapIndexed { y, line ->
-            line.mapIndexedNotNull { x, c ->
-                when (c) {
-                    '#' -> Triple(x, y, 0)
-                    else -> null
-                }
-            }
-        }.toSet()
+private data class Point4(val x: Int, val y: Int, val z: Int, val w: Int)
+private typealias State4 = Set<Point4>
+
+private fun State4.next4(): State4 {
+    val thisState = this
+    val nextState = mutableSetOf<Point4>()
+    val queue: Queue<Point4> = ArrayDeque<Point4>().apply {
+        addAll(thisState)
+    }
+    val prevQueued = mutableSetOf<Point4>().apply {
+        addAll(thisState)
     }
 
-    var state = initialState
+    while (queue.isNotEmpty()) {
+        queue.poll().let { point ->
+            val (x, y, z, w) = point
+            val activeNeighbors = (-1..1).sumOf { dx ->
+                (-1..1).sumOf { dy ->
+                    (-1..1).sumOf { dz ->
+                        (-1..1).count { dw ->
+                            val neighbor = Point4(x + dx, y + dy, z + dz, w + dw)
+                            if (point in thisState && neighbor !in prevQueued) {
+                                queue.add(neighbor)
+                                prevQueued.add(neighbor)
+                            }
+                            neighbor != point && neighbor in thisState
+                        }
+                    }
+                }
+            }
+
+
+            when {
+                point in thisState && (activeNeighbors in setOf(2, 3)) ->
+                    nextState.add(point)
+                point !in thisState && activeNeighbors == 3 ->
+                    nextState.add(point)
+                else -> {} // don't add to next state
+            }
+        }
+    }
+
+    return nextState
+}
+
+fun main() {
+    val initialState = mutableSetOf<Point>()
+    val initialState4 = mutableSetOf<Point4>()
+    (DATAPATH / "day17.txt").useLines { lines ->
+        lines.forEachIndexed { y, line ->
+            line.forEachIndexed { x, c ->
+                if (c == '#') {
+                    initialState.add(Point(x, y, 0))
+                    initialState4.add(Point4(x, y, 0, 0))
+                }
+            }
+        }
+    }
+
+    var state: State = initialState
     repeat(6) {
         state = state.next()
     }
     println("Part one: ${state.size}")
+
+    var state4: State4 = initialState4
+    repeat(6) {
+        state4 = state4.next4()
+    }
+    println("Part two: ${state4.size}")
 }
