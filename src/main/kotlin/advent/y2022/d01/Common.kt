8aaea86b529082e88ba3520eec649ec66a33f99b
Alex Grounds <agrounds@ojolabs.com>
2022-12-01 14:11:08 -0600

    2022 day 1 solution

---
+++ src/main/kotlin/advent/y2022/d01/Common.kt
@@ -0,0 +1,5 @@
+package advent.y2022.d01
+
+import kotlin.io.path.Path
+
+val DATAPATH = Path("${System.getProperty("user.home")}/data/advent-of-code/2022")
---
+++ src/main/kotlin/advent/y2022/d01/Day01.kt
@@ -0,0 +1,19 @@
+package advent.y2022.d01
+
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+
+fun main() {
+    val sums = (DATAPATH / "day01.txt").useLines { lines ->
+        lines.toList().fold(mutableListOf(0)) { sums, line ->
+            if (line.isEmpty()) {
+                sums.add(0)
+            } else {
+                sums[sums.size - 1] = sums.last() + line.toInt()
+            }
+            sums
+        }
+    }
+    println(sums.sorted().takeLast(3).sum())
+}
