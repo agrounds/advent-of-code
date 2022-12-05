19619045f58ab9628abbe9710b1243eb746ce2f4
Alex Grounds <agrounds90@gmail.com>
2022-12-05 09:55:13 -0600

    2015 day 1 solution

---
+++ src/main/kotlin/advent/y2015/Common.kt
@@ -0,0 +1,5 @@
+package advent.y2015
+
+import kotlin.io.path.Path
+
+val DATAPATH = Path("${System.getProperty("user.home")}/data/advent-of-code/2015")
---
+++ src/main/kotlin/advent/y2015/d01/Day01.kt
@@ -0,0 +1,33 @@
+package advent.y2015.d01
+
+import advent.y2015.DATAPATH
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+
+fun main() {
+    val line = (DATAPATH / "day01.txt").useLines { lines ->
+        lines.first()
+    }
+    line
+        .filter { it == '(' || it == ')' }
+        .partition { it == '(' }
+        .also { (opens, closes) ->
+            println("Part one: ${opens.length - closes.length}")
+        }
+    var position = 0
+    var firstBasement: Int? = null
+    line.onEachIndexed { index, c ->
+        when (c) {
+            '(' -> position++
+            ')' -> {
+                position--
+                if (position < 0 && firstBasement == null) {
+                    firstBasement = index
+                }
+            }
+        }
+    }
+    firstBasement = firstBasement?.inc()
+    println("Part two: $firstBasement")
+}
