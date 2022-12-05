a9109abd70252dd9298b99e246a0f8c377e45c35
Alex Grounds <agrounds90@gmail.com>
2022-12-05 13:33:30 -0600

    Support constant sources in all wiring directives

--- src/main/kotlin/advent/y2015/d07/Day07.kt
+++ src/main/kotlin/advent/y2015/d07/Day07.kt
@@ -11,64 +11,71 @@ infix fun Signal.shiftLeft(amount: Int) = shl(amount) % MAX_SIGNAL
 infix fun Signal.shiftRight(amount: Int) = shr(amount)
 fun Signal.not(): Signal = inv() % MAX_SIGNAL
 
+sealed class Source
+data class ConstantSource(val value: Signal): Source()
+data class WireSource(val value: String): Source()
+fun String.toSource(): Source =
+    toUIntOrNull()?.let { ConstantSource(it) } ?: WireSource(this)
+
 sealed class Wiring
 
-data class ConstWiring(val value: Signal): Wiring()
-data class DirectWiring(val source: String): Wiring()
-data class AndWiring(val source1: String, val source2: String): Wiring()
-data class OrWiring(val source1: String, val source2: String): Wiring()
-data class LShiftWiring(val source: String, val amount: Int): Wiring()
-data class RShiftWiring(val source: String, val amount: Int): Wiring()
-data class NotWiring(val source: String): Wiring()
+data class DirectWiring(val source: Source): Wiring()
+data class AndWiring(val source1: Source, val source2: Source): Wiring()
+data class OrWiring(val source1: Source, val source2: Source): Wiring()
+data class LShiftWiring(val source: Source, val amount: Int): Wiring()
+data class RShiftWiring(val source: Source, val amount: Int): Wiring()
+data class NotWiring(val source: Source): Wiring()
 
 fun parseWiring(line: String): Pair<String, Wiring> {
     val parts = line.split(" ")
-    val wiring = when {
-        parts[0].toIntOrNull() != null ->
-            ConstWiring(parts[0].toUInt())
-        parts[0] == "NOT" ->
-            NotWiring(parts[1])
-        parts[1] == "AND" ->
-            AndWiring(parts[0], parts[2])
-        parts[1] == "OR" ->
-            OrWiring(parts[0], parts[2])
-        parts[1] == "LSHIFT" ->
-            LShiftWiring(parts[0], parts[2].toInt())
-        parts[1] == "RSHIFT" ->
-            RShiftWiring(parts[0], parts[2].toInt())
-        else ->
-            DirectWiring(parts[0])
+
+    val wiring = when (parts.size) {
+        3 -> DirectWiring(parts[0].toSource())
+        4 -> NotWiring(parts[1].toSource())
+        5 ->
+            when (parts[1]) {
+                "AND" -> AndWiring(parts[0].toSource(), parts[2].toSource())
+                "OR" -> OrWiring(parts[0].toSource(), parts[2].toSource())
+                "LSHIFT" -> LShiftWiring(parts[0].toSource(), parts[2].toInt())
+                "RSHIFT" -> RShiftWiring(parts[0].toSource(), parts[2].toInt())
+                else -> throw RuntimeException("Invalid wiring: $line")
+            }
+        else -> throw RuntimeException("Invalid wiring: $line")
     }
     return parts.last() to wiring
 }
 
-fun partOne(wiringMap: Map<String, Wiring>, wire: String): Signal {
-    val signals: MutableMap<String, Signal> = mutableMapOf()
+fun resolveWireA(wiringMap: Map<String, Wiring>, initialSignals: Map<String, Signal>): Signal {
+    val signals: MutableMap<String, Signal> = initialSignals.toMutableMap()
 
-    fun resolveWire(wire: String): Signal {
-        if (wire in signals) return signals[wire]!!
-        val resolvedSignal: Signal = when (val wiring = wiringMap[wire]) {
-            is ConstWiring ->
-                wiring.value
-            is DirectWiring ->
-                resolveWire(wiring.source)
-            is AndWiring ->
-                resolveWire(wiring.source1) and resolveWire(wiring.source2)
-            is OrWiring ->
-                resolveWire(wiring.source1) or resolveWire(wiring.source2)
-            is LShiftWiring ->
-                resolveWire(wiring.source) shiftLeft wiring.amount
-            is RShiftWiring ->
-                resolveWire(wiring.source) shiftRight  wiring.amount
-            is NotWiring ->
-                resolveWire(wiring.source).not()
-            null ->
-                throw RuntimeException("Missing signal for wire $wire")
+    fun resolveSignal(source: Source): Signal = when (source) {
+        is ConstantSource -> source.value
+        is WireSource -> {
+            val wire = source.value
+            if (source.value in signals) signals[wire]!!
+            else {
+                val resolvedSignal: Signal = when (val wiring = wiringMap[wire]) {
+                    is DirectWiring ->
+                        resolveSignal(wiring.source)
+                    is AndWiring ->
+                        resolveSignal(wiring.source1) and resolveSignal(wiring.source2)
+                    is OrWiring ->
+                        resolveSignal(wiring.source1) or resolveSignal(wiring.source2)
+                    is LShiftWiring ->
+                        resolveSignal(wiring.source) shiftLeft wiring.amount
+                    is RShiftWiring ->
+                        resolveSignal(wiring.source) shiftRight wiring.amount
+                    is NotWiring ->
+                        resolveSignal(wiring.source).not()
+                    null ->
+                        throw RuntimeException("Missing signal for wire $wire")
+                }
+                signals[wire] = resolvedSignal
+                resolvedSignal
+            }
         }
-        signals[wire] = resolvedSignal
-        return resolvedSignal
     }
-    return resolveWire(wire)
+    return resolveSignal(WireSource("a"))
 }
 
 
@@ -76,6 +83,8 @@ fun main() {
     val wiringMap: Map<String, Wiring> = (DATAPATH / "day07.txt").useLines { lines ->
         lines.toList().associate(::parseWiring)
     }
-    partOne(wiringMap, "a")
+    resolveWireA(wiringMap, emptyMap())
         .also { println("Part one: $it") }
+    resolveWireA(wiringMap, mapOf("b" to 16076.toUInt()))
+        .also { println("Part two: $it") }
 }
