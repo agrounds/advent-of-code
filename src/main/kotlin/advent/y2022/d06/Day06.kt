6e7a7455e4298ada895dd91b1be7d4991d780f9d
Alex Grounds <agrounds90@gmail.com>
2022-12-06 08:41:53 -0600

    2022 day 6 solution

---
+++ src/main/kotlin/advent/y2022/d06/Day06.kt
@@ -0,0 +1,26 @@
+package advent.y2022.d06
+
+import advent.DATAPATH
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+fun seekPacketStart(buffer: String, distinctLength: Int): Int? {
+    var i = 0
+    while (i + distinctLength <= buffer.length) {
+        if (buffer.substring(i, i + distinctLength).toSet().size == distinctLength) {
+            return i + distinctLength
+        }
+        i++
+    }
+    return null
+}
+
+
+
+fun main() {
+    (DATAPATH / "2022/day06.txt").useLines { lines ->
+        lines.first()
+    }
+        .also { println("Part one: ${seekPacketStart(it, 4)}") }
+        .also { println("Part two: ${seekPacketStart(it, 14)}") }
+}
