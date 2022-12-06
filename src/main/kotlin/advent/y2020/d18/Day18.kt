75ec3e384ed613dd6f92bea4407516973de0b483
Alex Grounds <agrounds90@gmail.com>
2022-12-06 09:42:11 -0600

    Tokenize input to 2020 day 18

--- src/main/kotlin/advent/y2020/d18/Day18.kt
+++ src/main/kotlin/advent/y2020/d18/Day18.kt
@@ -1,19 +1,89 @@
 package advent.y2020.d18
 
 import advent.DATAPATH
+import java.lang.RuntimeException
 import kotlin.io.path.div
 import kotlin.io.path.useLines
 
-private fun evaluate(expression: String, from: Int? = null, to: Int? = null): Long {
-    var curr = from ?: 0
+enum class Operation {
+    PLUS,
+    TIMES;
 
-
-    return 0
+    fun eval(a: Int, b: Int): Int = when (this) {
+        PLUS -> a + b
+        TIMES -> a * b
+    }
 }
 
-fun main() {
-    val expressions = (DATAPATH / "2020/day18.txt").useLines { it.toList() }
+sealed class Token
 
-    expressions.sumOf { evaluate(it) }
-        .also { println("Part one: $it") }
+data class TokenList(val tokens: List<Token>) : Token()
+data class LitToken(val value: Int) : Token()
+data class OpToken(val op: Operation) : Token()
+
+// from inclusive, to exclusive
+fun seekClosingParen(line: String, from: Int, to: Int): Int? {
+    var parenCount = 1
+    var i = from
+    while (i < to) {
+        when (line[i]) {
+            '(' -> parenCount++
+            ')' -> {
+                parenCount--
+                if (parenCount == 0) return i
+            }
+        }
+        i++
+    }
+    return null
+}
+
+// from inclusive, to exclusive
+fun parseLine(line: String, from: Int? = null, to: Int? = null): Token {
+    var i = from ?: 0
+    val stop = to ?: line.length
+    val tokens = mutableListOf<Token>()
+
+    while (i < stop) {
+        when {
+            line[i] == ' ' ->
+                i++
+            line[i] in "0123456789" -> {
+                var j = i + 1
+                while (j < stop && line[j] in "0123456789") j++
+                tokens.add(LitToken(line.substring(i, j).toInt()))
+                i = j
+            }
+            line[i] == '+' -> {
+                tokens.add(OpToken(Operation.PLUS))
+                i++
+            }
+            line[i] == '*' -> {
+                tokens.add(OpToken(Operation.TIMES))
+                i++
+            }
+            line[i] == '(' -> {
+                val j = seekClosingParen(line, i+1, stop) ?: throw RuntimeException("Unbalanced parentheses")
+                tokens.add(parseLine(line, i+1, j))
+                // skip close paren character
+                i = j+1
+            }
+            else ->
+                throw RuntimeException("Unexpected token ${line[i]} at position $i")
+        }
+    }
+
+    return when (tokens.size) {
+        0 -> throw RuntimeException("Missing tokens in substring ${line.substring(i, stop)}, chars $i to $stop")
+        1 -> tokens.first()
+        else -> TokenList(tokens)
+    }
+}
+
+
+fun main() {
+    val tokenizedLines: List<Token> = (DATAPATH / "2020/day18.txt").useLines { lines ->
+        lines.toList()
+            .map { parseLine(it) }
+    }
 }
