3d26b1b68cbd9cbe9604ae1cc1669cb0c6f9b74b
Alex Grounds <agrounds@ojolabs.com>
2022-12-06 15:00:26 -0600

    Refactor to not recursively resolve rules, use lookups instead

--- src/main/kotlin/advent/y2020/d19/Rule.kt
+++ src/main/kotlin/advent/y2020/d19/Rule.kt
@@ -1,37 +1,28 @@
 package advent.y2020.d19
 
-sealed class Rule {
-    abstract fun matches(string: String): Boolean
-}
-data class LiteralRule(val literal: String) : Rule() {
-    override fun toString() = literal
-    override fun matches(string: String): Boolean =
-        string == literal
-}
-data class SequenceRule(val sequence: List<Rule>) : Rule() {
-    override fun toString() = "(${sequence.joinToString("")})"
-    override fun matches(string: String): Boolean {
-        fun subMatches(stringStart: Int, seqStart: Int): Boolean {
-            val seq = sequence
-            if (stringStart == string.length && seqStart == sequence.size) {
-                return true
+sealed class Rule
+data class LiteralRule(val literal: String) : Rule()
+data class SequenceRule(val rules: List<Int>) : Rule()
+data class OrRule(val leftRules: List<Int>, val rightRules: List<Int>) : Rule()
+
+class RuleParser(private val ruleStrings: List<String>) {
+    private val parsedRules = mutableMapOf<Int, Rule>()
+
+    init {
+        ruleStrings.forEach { ruleString ->
+            val colonIdx = ruleString.indexOf(':')
+            val n = ruleString.substring(0 until colonIdx).toInt()
+            val parts = ruleString.substring(colonIdx + 1).split(" ")
+            val firstPart = parts.first()
+            parsedRules[n] = when {
+                parts.size == 1 && firstPart[0] == '"' ->
+                    LiteralRule(firstPart.substring(1 until firstPart.length - 1))
+                "|" in parts ->
+                    OrRule(parts.takeWhile { it != "|" }.map { it.toInt() },
+                        parts.takeLastWhile { it != "|" }.map { it.toInt() })
+                else ->
+                    SequenceRule(parts.map { it.toInt() })
             }
-            // out of rules, but characters in the string remain
-            if (seqStart >= sequence.size) {
-                return false
-            }
-            for (i in stringStart+1..string.length) {
-                if (sequence[seqStart].matches(string.substring(stringStart, i)))
-                    if (subMatches(i, seqStart+1))
-                        return true
-            }
-            return false
         }
-        return subMatches(0, 0)
     }
-}
-data class OrRule(val a: Rule, val b: Rule) : Rule() {
-    override fun toString() = "$a|$b"
-    override fun matches(string: String): Boolean =
-        a.matches(string) || b.matches(string)
 }
