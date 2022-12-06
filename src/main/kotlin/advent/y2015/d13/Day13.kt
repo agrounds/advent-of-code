9529178693685b2626a5494390b43613d140de60
Alex Grounds <agrounds90@gmail.com>
2022-12-06 08:57:02 -0600

    2015 day 13 solution

--- src/main/kotlin/advent/y2015/d13/Day13.kt
+++ src/main/kotlin/advent/y2015/d13/Day13.kt
@@ -5,9 +5,20 @@ import kotlin.io.path.div
 import kotlin.io.path.useLines
 
 fun maxHappiness(opinions: Array<IntArray>): Int {
-    var max = 0
+    // happiness  - total happiness from seatings so far
+    // prevPerson - most recent seated person
+    // needToSeat - set of people still needing to be seated
+    // the first and last person (circular table) is always 0 by convention
+    fun helper(happiness: Int, prevPerson: Int, needToSeat: Set<Int>): Int {
+        if (needToSeat.isEmpty()) {
+            return happiness + opinions[0][prevPerson] + opinions[prevPerson][0]
+        }
+        return needToSeat.maxOf { person ->
+            helper(happiness + opinions[prevPerson][person] + opinions[person][prevPerson], person, needToSeat - person)
+        }
+    }
 
-    return max
+    return helper(0, 0, (1 until opinions.size).toSet())
 }
 
 
@@ -28,4 +39,12 @@ fun main() {
             opinions[names.indexOf(person)][names.indexOf(neighbor)] = happiness
         }
     }
+    println("Part one: ${maxHappiness(opinions)}")
+    val opinions2 = Array(9) { IntArray(9) }
+    opinions.forEachIndexed { i, row ->
+        row.forEachIndexed { j, happiness ->
+            opinions2[i][j] = happiness
+        }
+    }
+    println("Part two: ${maxHappiness(opinions2)}")
 }
