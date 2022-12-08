7644961077d086e43d50002e8cbc484e64054c0d
Alex Grounds <agrounds90@gmail.com>
2022-12-08 09:32:17 -0600

    Build list of walks to take to avoid repeating the visibility checks

--- src/main/kotlin/advent/y2022/d08/Day08.kt
+++ src/main/kotlin/advent/y2022/d08/Day08.kt
@@ -8,40 +8,27 @@ fun countVisibleTrees(grid: List<List<Int>>): Int {
     val size = grid.size  // grid is a square, so rows and columns have same size
     val visible = Array(size) { BooleanArray(size) }
 
-    (0 until size).forEach { i ->
+    /*
+     * Collection of walks to take across the grid of trees. A walk is a list of
+     * coordinate pairs (i, j) of trees to consider, in a specified order.
+     */
+    (
+        // left to right
+        (0 until size).map { i ->
+            (0 until size).map { j -> i to j }
+        } +
+        // right to left
+        (0 until size).map { i ->
+            (size - 1 downTo 0).map { j -> i to j }
+        }
+    ).let { walks ->
+        // top to bottom and bottom to top
+        walks + walks.map { walk ->
+            walk.map { (i, j) -> j to i }
+        }
+    }.forEach { walk ->
         var maxTree = -1
-        // rows left-to-right
-        (0 until size).forEach { j ->
-            val tree = grid[i][j]
-            if (tree > maxTree) {
-                visible[i][j] = true
-                maxTree = tree
-            }
-        }
-        maxTree = -1
-        // rows right-to-left
-        (size-1 downTo 0).forEach { j ->
-            val tree = grid[i][j]
-            if (tree > maxTree) {
-                visible[i][j] = true
-                maxTree = tree
-            }
-        }
-    }
-
-    (0 until size).forEach { j ->
-        var maxTree = -1
-        // columns top-to-bottom
-        (0 until size).forEach { i ->
-            val tree = grid[i][j]
-            if (tree > maxTree) {
-                visible[i][j] = true
-                maxTree = tree
-            }
-        }
-        maxTree = -1
-        // columns bottom-to-top
-        (size-1 downTo 0).forEach { i ->
+        walk.forEach { (i, j) ->
             val tree = grid[i][j]
             if (tree > maxTree) {
                 visible[i][j] = true
