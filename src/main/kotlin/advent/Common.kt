28e7ae54ea09ef09e605fa69a1ca3b5499f8eb8e
Alex Grounds <agrounds90@gmail.com>
2022-12-08 11:40:44 -0600

    Implement 2022 day 8 part two

--- src/main/kotlin/advent/Common.kt
+++ src/main/kotlin/advent/Common.kt
@@ -1,9 +1,14 @@
 package advent
 
+import java.util.Stack
 import kotlin.io.path.Path
 import kotlin.math.pow
 
 
 fun Int.pow(p: Int): Long = this.toDouble().pow(p).toLong()
+
+fun <T> Stack<T>.peekOrNull(): T? =
+    if (isEmpty()) null
+    else peek()
 
 val DATAPATH = Path("${System.getProperty("user.home")}/data/advent-of-code")
--- src/main/kotlin/advent/y2022/d08/Day08.kt
+++ src/main/kotlin/advent/y2022/d08/Day08.kt
@@ -1,22 +1,21 @@
 package advent.y2022.d08
 
 import advent.DATAPATH
+import advent.peekOrNull
+import kotlin.math.abs
+import java.util.Stack
 import kotlin.io.path.div
 import kotlin.io.path.useLines
 
-fun countVisibleTrees(grid: List<List<Int>>): Int {
-    val size = grid.size  // grid is a square, so rows and columns have same size
-    val visible = Array(size) { BooleanArray(size) }
-
-    /*
-     * Collection of walks to take across the grid of trees. A walk is a list of
-     * coordinate pairs (i, j) of trees to consider, in a specified order.
-     */
-    (
-        // left to right
-        (0 until size).map { i ->
-            (0 until size).map { j -> i to j }
-        } +
+/**
+ * Collection of walks to take across the grid of trees. A walk is a list of
+ * coordinate pairs (i, j) of trees to consider, in a specified order.
+ */
+fun getWalks(size: Int): List<List<Pair<Int, Int>>> = (
+    // left to right
+    (0 until size).map { i ->
+        (0 until size).map { j -> i to j }
+    } +
         // right to left
         (0 until size).map { i ->
             (size - 1 downTo 0).map { j -> i to j }
@@ -26,7 +25,17 @@ fun countVisibleTrees(grid: List<List<Int>>): Int {
         walks + walks.map { walk ->
             walk.map { (i, j) -> j to i }
         }
-    }.forEach { walk ->
+    }
+
+fun countVisibleTrees(grid: List<List<Int>>): Int {
+    val size = grid.size  // grid is a square, so rows and columns have same size
+    val visible = Array(size) { BooleanArray(size) }
+
+    /*
+     * Collection of walks to take across the grid of trees. A walk is a list of
+     * coordinate pairs (i, j) of trees to consider, in a specified order.
+     */
+    getWalks(size).forEach { walk ->
         var maxTree = -1
         walk.forEach { (i, j) ->
             val tree = grid[i][j]
@@ -40,6 +49,66 @@ fun countVisibleTrees(grid: List<List<Int>>): Int {
     return visible.sumOf { row -> row.count { it } }
 }
 
+fun maxScenicScore(grid: List<List<Int>>): Int {
+    val size = grid.size
+    val scores = Array(size) { IntArray(size) { 1 } }
+
+    infix fun Pair<Int, Int>.compareHeight(other: Pair<Int, Int>): Int {
+        val (thisX, thisY) = this
+        val (otherX, otherY) = other
+        return grid[thisX][thisY] - grid[otherX][otherY]
+    }
+
+    getWalks(size).forEach { walk ->
+        val prevMaxes = Stack<Pair<Int, Int>>()
+        val xDirection = walk[0].first != walk[1].first
+        val increasing =
+            if (xDirection) walk[0].first < walk[1].first
+            else walk[0].second < walk[1].second
+
+        infix fun Pair<Int, Int>.diff(other: Pair<Int, Int>?): Int {
+            val (thisX, thisY) = this
+            if (other == null) {
+                return when {
+                    xDirection && increasing -> thisX
+                    xDirection && !increasing -> size - thisX - 1
+                    !xDirection && increasing -> thisY
+                    else -> size - thisY - 1
+                }
+            }
+            val (otherX, otherY) = other
+
+            return if (xDirection) abs(thisX - otherX)
+            else abs(thisY - otherY)
+        }
+
+        walk.forEach { point ->
+            val (i, j) = point
+            var prevMax: Pair<Int, Int>? = null
+            while (prevMaxes.isNotEmpty() && point compareHeight prevMaxes.peek() >= 0) {
+                prevMax = prevMaxes.pop()
+            }
+            when {
+                // we encountered another tree of same height as this one, so
+                // the score is the distance from this tree to the previous one of same height
+                prevMax != null && point compareHeight prevMax == 0 -> {
+                    scores[i][j] *= point diff prevMax
+                }
+                // either we're the tallest tree so far, or prevMax is a tree shorter than this
+                // one. the score is the distance from this tree to the previous _strictly_ taller
+                // tree (which is prevMaxes.peek()), or this is the tallest tree so far and we should
+                // diff null. point diff peekOrNull() handles both cases.
+                else -> {
+                    scores[i][j] *= point diff prevMaxes.peekOrNull()
+                }
+            }
+            prevMaxes.push(point)
+        }
+    }
+
+    return scores.maxOf { it.maxOrNull()!! }
+}
+
 
 fun main() {
     val grid = (DATAPATH / "2022/day08.txt").useLines { lines ->
@@ -47,5 +116,8 @@ fun main() {
             line.map { it - '0' }
         }
     }
-    countVisibleTrees(grid).also { println("Part one: $it") }
+    countVisibleTrees(grid)
+        .also { println("Part one: $it") }
+    maxScenicScore(grid)
+        .also { println("Part two: $it") }
 }
