3e6f005bdbe4800e68018a18fb8a891575b2d8af
Alex Grounds <agrounds90@gmail.com>
2022-12-08 13:29:20 -0600

    Refactor to find edge and corner pieces first

--- src/main/kotlin/advent/y2020/d20/Day20.kt
+++ src/main/kotlin/advent/y2020/d20/Day20.kt
@@ -1,38 +1,56 @@
 package advent.y2020.d20
 
 import advent.DATAPATH
-import advent.sqrt
 import kotlin.io.path.div
 import kotlin.io.path.useLines
 
 typealias Tile = List<String>
+
+/**
+ * Returns all edges of this tile, read both clockwise (cw)
+ * and counterclockwise (ccw). The order is:
+ *   cw top, cw right, cw bottom, cw left,
+ *   ccw top, ccw right, ccw bottom, ccw left
+ */
 fun Tile.edges(): List<String> = listOf(
-    // all edges read in clockwise direction
-    this.first(),  // top
-    String(this.map { it.last() }.toCharArray()),  // right
-    this.last().reversed(),  // bottom
-    String(this.reversed().map { it.first() }.toCharArray()),  // left
-)
+    this.first(),
+    String(this.map { it.last() }.toCharArray()),
+    this.last().reversed(),
+    String(this.reversed().map { it.first() }.toCharArray()),
+).let { edges ->
+    edges + edges.map { it.reversed() }
+}
+fun Tile.flip(): Tile = TODO()
+fun Tile.rotate(times: Int): Tile = TODO()
 
-fun solvePuzzle(tilesMap: Map<Int, Tile>) {
-    // the tiles will assemble into a square image, so there are a
-    // square number of them
-    val sideLen = sqrt(tilesMap.size)
-    // a table on which to lay down tiles to solve the puzzle. it
-    // is made big enough that any piece may be placed in the middle,
-    // and the table will have enough space to solve it from there.
-    val table = Array(sideLen * 2 - 1) { Array<Tile?>(sideLen * 2 - 1) { null } }
+/**
+ * Order matters. Here, we optionally flip across the vertical axis, then rotate
+ * some number of times clockwise.
+ */
+data class TileOrientation(val num: Int, val flip: Boolean, val rotate: Int)
 
+fun findEdgeCornerTiles(tilesMap: Map<Int, Tile>): Pair<List<Int>, List<Int>> {
     // map from an edge to all tiles having it as an edge, possibly flipped
-    val edges: MutableMap<String, MutableSet<Int>> = mutableMapOf()
+    val tileEdges: MutableMap<String, MutableSet<Int>> = mutableMapOf()
     tilesMap.forEach { (num, tile) ->
         tile.edges().let { edges ->
             edges + edges.map { it.reversed() }
         }.forEach { edge ->
-            (edges[edge] ?: mutableSetOf<Int>().also { edges[edge] = it })
+            (tileEdges[edge] ?: mutableSetOf<Int>().also { tileEdges[edge] = it })
                 .add(num)
         }
     }
+
+    return tileEdges.values
+        .filter { it.size == 1 }
+        .map { it.first() }
+        .groupBy { it }
+        .mapValues { (_, v) -> v.size }.entries
+        // edge tiles appear twice here, and corners appear four times
+        .partition { (_, count) -> count == 2 }
+        .let { (edgeTiles, cornerTiles) ->
+            edgeTiles.map { it.key } to cornerTiles.map { it.key }
+        }
 }
 
 
@@ -44,5 +62,8 @@ fun main() {
         }
         map
     }
-    solvePuzzle(tilesMap)
+    val (edgeTiles, cornerTiles) = findEdgeCornerTiles(tilesMap)
+    cornerTiles.map { it.toLong() }
+        .reduce { a, b -> a * b }
+        .also{ println("Part one: $it") }
 }
