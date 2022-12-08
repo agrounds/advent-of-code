ea1e127a77e2c8ea6504852601ffc32c77ea387b
Alex Grounds <agrounds90@gmail.com>
2022-12-08 14:46:28 -0600

    Place first tile of each row based on tile placed above

--- src/main/kotlin/advent/y2020/d20/Day20.kt
+++ src/main/kotlin/advent/y2020/d20/Day20.kt
@@ -39,22 +39,22 @@ class Solver(private val tilesMap: Map<Int, Tile>) {
     // a table on which to lay down tiles to solve the puzzle. it
     // is just big enough to store the solved puzzle, so table[0][0]
     // must be a corner piece in correct orientation
-    private val table = Array(sideLen) { Array<Tile?>(sideLen) { null } }
+    private val table = Array(sideLen) { Array<TileOrientation?>(sideLen) { null } }
 
     val cornerTiles: List<Int>
     private val puzzleEdges: Set<String>
+    private val tilesByEdge: Map<String, Set<Int>>
 
     init {
         // map from an edge to all tiles having it as an edge, possibly flipped
         val tileEdges = mutableMapOf<String, MutableSet<Int>>()
         tilesMap.forEach { (num, tile) ->
-            tile.edges().let { edges ->
-                edges + edges.map { it.reversed() }
-            }.forEach { edge ->
+            tile.edges().forEach { edge ->
                 (tileEdges[edge] ?: mutableSetOf<Int>().also { tileEdges[edge] = it })
                     .add(num)
             }
         }
+        tilesByEdge = tileEdges
 
         cornerTiles = tileEdges
             .filterValues { it.size == 1 }
@@ -71,8 +71,70 @@ class Solver(private val tilesMap: Map<Int, Tile>) {
             }
     }
 
-    fun solvePuzzle() {
+    /**
+     * Returns all edges of this tile, post transformation, read
+     * both clockwise (cw) and counterclockwise (ccw). The order is:
+     *   cw top, cw right, cw bottom, cw left,
+     *   ccw top, ccw right, ccw bottom, ccw left
+     */
+    private fun TileOrientation.edges(): List<String>  =
+        tilesMap[num]!!.edges().take(4).let { unflippedEdges ->
+            if (flip) {
+                unflippedEdges
+                    // cw edges become ccw edges
+                    .map { it.reversed() }
+                    // the order changes to what was originally top, left, bottom, right
+                    .let { reversedEdges ->
+                        (0 until 4).map { reversedEdges[(4 - it) % 4] }
+                    }
+            } else {
+                unflippedEdges
+            }
+        }.let { unrotatedEdges ->
+            // perform rotation
+            (0 until 4).map { unrotatedEdges[(it + 4 - rotate) % 4] }
+        }.let { edges ->
+            edges + edges.map { it.reversed() }
+        }
 
+    fun solvePuzzle() {
+        val firstCorner = cornerTiles[0]
+        tilesMap[firstCorner]!!.edges()
+            .take(4)
+            .map { it in puzzleEdges }
+            .let { (t, r, b, l) ->
+                val firstCornerTurns = when {
+                    l && t -> 0
+                    t && r -> 3
+                    r && b -> 2
+                    b && l -> 1
+                    else -> throw RuntimeException("First corner $firstCorner has unexpected puzzle edge overlap")
+                }
+                table[0][0] = TileOrientation(firstCorner, false, firstCornerTurns)
+            }
+
+        (0 until sideLen).forEach { i ->
+            if (i != 0) {
+                // place tile based on the one above this spot
+                val tileAbove = table[i-1][0] ?: throw RuntimeException("Tile not placed at (${i-1}, 0)!")
+                val bottomEdge = tileAbove.edges()[2]
+                val nextTile = tilesByEdge[bottomEdge]!!
+                    .filterNot { it == tileAbove.num }
+                    .first()
+                table[i][0] = tilesMap[nextTile]!!
+                    .edges()
+                    .indexOf(bottomEdge)
+                    .let { matchingEdgeIdx ->
+                        // if tileAbove's CW bottom edge matches one of our CW edges, then a flip is required
+                        if (matchingEdgeIdx < 4) TileOrientation(nextTile, true, matchingEdgeIdx)
+                        // if tileAbove's CW bottom edge matches one of our CCW edges, then no flip is required
+                        else TileOrientation(nextTile, false, (8 - matchingEdgeIdx) % 4)
+                    }
+            }
+            (1 until sideLen).forEach { j ->
+                // place tile based on the one to the left of this spot
+            }
+        }
     }
 }
 
