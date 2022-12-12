bd50fe640d406002f1af1879b6b27a3aa8eadf1d
Alex Grounds <agrounds90@gmail.com>
2022-12-12 09:51:17 -0600

    Implement Dijkstra's algorithm to find shortest path up hill

--- src/main/kotlin/advent/y2022/d12/Day12.kt
+++ src/main/kotlin/advent/y2022/d12/Day12.kt
@@ -4,28 +4,79 @@ import advent.DATAPATH
 import kotlin.io.path.div
 import kotlin.io.path.useLines
 
+typealias Point = Pair<Int, Int>
+val Point.x: Int
+    get() = this.first
+val Point.y: Int
+    get() = this.second
+
+fun Point.left(): Point = this.copy(first = first-1)
+fun Point.right(): Point = this.copy(first = first+1)
+fun Point.up(): Point = this.copy(second = second-1)
+fun Point.down(): Point = this.copy(second = second+1)
+
 typealias Grid = List<List<Int>>
-fun Grid.height(x: Int, y: Int): Int = this[y][x]
+fun Grid.height(point: Point): Int? = point.let { (x, y) ->
+    when {
+        y !in this.indices -> null
+        x !in this[y].indices -> null
+        else -> this[y][x]
+    }
+}
+
+class Solver(private val grid: Grid, start: Point) {
+    private val toVisit = mutableSetOf(start)
+    private val visited = mutableSetOf<Point>()
+    private val distances = Array(grid.size) { IntArray(grid.first().size) { Integer.MAX_VALUE } }
+        .apply { this[start.y][start.x] = 0 }
+
+    private fun neighbors(point: Point): List<Point> =
+        listOf(point.left(), point.right(), point.up(), point.down())
+            .filter { neighbor ->
+                grid.height(neighbor).let {
+                    it != null && it <= grid.height(point)!! + 1
+                }
+            }
+
+    private fun currDistance(point: Point): Int = distances[point.y][point.x]
+
+    fun minDistance(to: Point): Int {
+        while (to !in visited) {
+            val next = toVisit.minByOrNull(::currDistance)
+                ?: throw RuntimeException("Set of points to visit is empty even though graph is not fully explored!")
+            toVisit.remove(next)
+            neighbors(next)
+                .filterNot { it in visited }
+                .forEach { neighbor ->
+                    distances[neighbor.y][neighbor.x] = minOf(
+                        currDistance(neighbor),
+                        currDistance(next) + 1
+                    )
+                    toVisit.add(neighbor)
+                }
+
+            visited.add(next)
+
+        }
+        return currDistance(to)
+    }
+}
 
 
 fun main() {
-    var startX = 0
-    var startY = 0
-    var endX = 0
-    var endY = 0
+    var start = 0 to 0
+    var end = 0 to 0
 
     val grid: Grid = (DATAPATH / "2022/day12.txt").useLines { lines ->
         lines.toList().mapIndexed { y, line ->
             line.mapIndexed { x, c ->
                 when (c) {
                     'S' -> {
-                        startX = x
-                        startY = y
+                        start = x to y
                         0
                     }
                     'E' -> {
-                        endX = x
-                        endY = y
+                        end = x to y
                         25
                     }
                     else ->
@@ -34,4 +85,7 @@ fun main() {
             }
         }
     }
+    val solver = Solver(grid, start)
+    solver.minDistance(end)
+        .also { println("Part one: $it") }
 }
