3a2e1f758daaa728918310ba7db17d688dcfae9e
Alex Grounds <agrounds90@gmail.com>
2022-12-14 09:17:43 -0600

    Implement 2022 day 14 part one

---
+++ src/main/kotlin/advent/y2022/d14/Day14.kt
@@ -0,0 +1,84 @@
+package advent.y2022.d14
+
+import advent.DATAPATH
+import kotlin.io.path.div
+import kotlin.io.path.useLines
+
+data class Point(val x: Int, val y: Int)
+fun String.toPoint(): Point = split(",").let { (a, b) ->
+    Point(a.toInt(), b.toInt())
+}
+
+class Solver(input: Sequence<String>) {
+    private val blockedPoints: Set<Point>
+    private val maxDepth: Int
+
+    init {
+        val tmpBlockedPoints = mutableSetOf<Point>()
+        var tmpMaxDepth = 0
+        input.forEach { line ->
+            var prevPoint: Point? = null
+            line.split(" -> ")
+                .map { it.toPoint() }
+                .forEach { point ->
+                    tmpMaxDepth = maxOf( tmpMaxDepth, point.y)
+                    val prevPoint1 = prevPoint
+                    if (prevPoint1 != null) {
+                        val fromX = minOf(point.x, prevPoint1.x)
+                        val toX = maxOf(point.x, prevPoint1.x)
+                        val fromY = minOf(point.y, prevPoint1.y)
+                        val toY = maxOf(point.y, prevPoint1.y)
+                        (fromX..toX).forEach { x ->
+                            (fromY..toY).forEach { y ->
+                                tmpBlockedPoints.add(Point(x, y))
+                            }
+                        }
+                    }
+                    prevPoint = point
+                }
+        }
+        blockedPoints = tmpBlockedPoints
+        maxDepth = tmpMaxDepth
+    }
+
+    fun simulateFallingSand(sandSource: Point): Int {
+        val bp = blockedPoints.toMutableSet()
+
+        fun simulateOne(): Boolean {
+            var sand = sandSource
+            while (true) {
+                if (sand.y > maxDepth) return false
+                when {
+                    sand.copy(y = sand.y + 1) !in bp -> {
+                        sand = sand.copy(y = sand.y + 1)
+                    }
+
+                    Point(sand.x - 1, sand.y + 1) !in bp -> {
+                        sand = Point(sand.x - 1, sand.y + 1)
+                    }
+
+                    Point(sand.x + 1, sand.y + 1) !in bp -> {
+                        sand = Point(sand.x + 1, sand.y + 1)
+                    }
+
+                    else -> {
+                        bp.add(sand)
+                        return true
+                    }
+                }
+            }
+        }
+
+        var sandAtRest = 0
+        while (simulateOne()) sandAtRest++
+        return sandAtRest
+    }
+}
+
+
+
+fun main() {
+    val solver = (DATAPATH / "2022/day14.txt").useLines { Solver(it) }
+    solver.simulateFallingSand(Point(500, 0))
+        .also { println("Part one: $it") }
+}
