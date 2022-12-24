a1761e4322cb744822dbbd7753b03e1c0203398f
Alex Grounds <agrounds90@gmail.com>
2022-12-23 21:12:03 -0600

    Compute max flow of any subset of open valves instead

--- src/main/kotlin/advent/y2022/d16/Day16.kt
+++ src/main/kotlin/advent/y2022/d16/Day16.kt
@@ -12,58 +12,113 @@ fun parseLine(line: String): Valve {
     return Valve(parts[1], flowRate, adjacentValves)
 }
 
-data class State(val position: String, val openValves: Set<String>, val timeRemaining: Int)
-fun State.moveTo(newPosition: String) = copy(position = newPosition, timeRemaining = timeRemaining - 1)
-fun State.openValve() = copy(openValves = openValves + position, timeRemaining = timeRemaining - 1)
-fun State.valveIsOpen() = position in openValves
-val START = State("AA", emptySet(), 30)
+const val START = "AA"
 
-fun maxFlow(valves: Map<String, Valve>): Int {
-    fun State.flowRate() = openValves.sumOf { valves[it]!!.flowRate }
+class Solver(valves: List<Valve>, private val time: Int) {
+    private val nameToValve = valves.associateBy { it.name }
+    private val nameToNumber = mutableMapOf<String, Int>()
+    private val numberToValve: Array<Valve>
+    private val distanceFromStart: IntArray
+    private val distances: Array<IntArray>
 
-    var maxFlow = 0
+    // The keys are bitmaps representing which valves are open.
+    // E.g. 0...01101 means that valves 0, 2, 3 are open, others closed.
+    // The value is the maximum amount of flow that can be released via some path
+    // that eventually opens this set of valves, in some order.
+    // Assumes that at most 32 valves can be opened!
+    private val maxFlows = mutableMapOf<Int, Int>()
 
-    val states = mutableMapOf<State, Int>()
-    val queue = ArrayDeque<State>()
-    states[START] = 0
-    queue.addFirst(START)
-
-    while (queue.isNotEmpty()) {
-        val currState = queue.removeFirst()
-        val currFlow = states[currState]!!
-        if (currState.timeRemaining == 0) {
-            maxFlow = maxOf(maxFlow, states[currState]!!)
-        } else {
-            val newFlow = currFlow + currState.flowRate()
-            if (!currState.valveIsOpen() && valves[currState.position]!!.flowRate > 0) {
-                val opened = currState.openValve()
-                if (opened in states) {
-                    states[opened] = maxOf(states[opened]!!, newFlow)
-                } else {
-                    states[opened] = newFlow
-                    queue.add(opened)
-                }
+    init {
+        // number the valves with nonzero flow rate
+        valves.forEach { valve ->
+            if (valve.flowRate > 0) {
+                nameToNumber[valve.name] = nameToNumber.size
             }
-            valves[currState.position]!!.adjacentValves.forEach { valve ->
-                val moved = currState.moveTo(valve)
-                if (moved in states) {
-                    states[moved] = maxOf(states[moved]!!, newFlow)
-                } else {
-                    states[moved] = newFlow
-                    queue.add(moved)
-                }
+        }
+        numberToValve = Array(nameToNumber.size) { Valve("dummy", 0, emptyList()) }.apply {
+            nameToNumber.forEach { (name, num) -> this[num] = nameToValve[name]!! }
+        }
+
+        // initialize distances
+        distanceFromStart = computeDistances(nameToValve[START]!!)
+        distances = Array(nameToNumber.size) {
+            computeDistances(numberToValve[it])
+        }
+
+        computeMaxFlows()
+    }
+
+    private fun computeDistances(start: Valve): IntArray {
+        val ret = IntArray(nameToNumber.size)
+        val queue = ArrayDeque(listOf(start to 0))
+        val visited = mutableSetOf<String>()
+        while (queue.isNotEmpty()) {
+            val (valve, dist) = queue.removeFirst()
+            visited.add(valve.name)
+            nameToNumber[valve.name]?.let { n ->
+                ret[n] = dist
             }
+            valve.adjacentValves
+                .filterNot { it in visited }
+                .forEach {
+                    queue.add(nameToValve[it]!! to (dist + 1))
+                }
+        }
+        return ret
+    }
+
+    private fun totalFlowRate(openValves: Int): Int =
+        numberToValve.mapIndexed { n, valve ->
+            if (openValves and (1 shl n) != 0) valve.flowRate
+            else 0
+        }.sum()
+
+    private fun computeMaxFlows() {
+        // openValves is a bitmap, same as maxFlows' keys
+        data class State(val position: Int, val openValves: Int, val timeRemaining: Int, val flowSoFar: Int)
+        fun State.goTo(newPosition: Int): State {
+            val actionTime = distances[position][newPosition] + 1
+            val newOpenValves = openValves or (1 shl newPosition)
+            return State(newPosition, newOpenValves,
+                timeRemaining - actionTime,
+                flowSoFar + totalFlowRate(openValves) * actionTime)
+        }
+
+        val queue = distanceFromStart
+            .mapIndexed { n, dist ->
+                State(n, 1 shl n, time - (dist + 1), 0).takeIf { it.timeRemaining >= 0 }
+            }
+            .filterNotNull()
+            .let(::ArrayDeque)
+
+        while (queue.isNotEmpty()) {
+            val curr = queue.removeFirst()
+            maxFlows[curr.openValves] = maxOf(
+                maxFlows[curr.openValves] ?: 0,
+                curr.flowSoFar + totalFlowRate(curr.openValves) * curr.timeRemaining
+            )
+            distances.indices
+                .filter { curr.openValves and (1 shl it) == 0 }  // do not go to an open valve
+                .filter { distances[curr.position][it] < curr.timeRemaining - 1 }  // do not go to a valve we don't have time to open
+                .forEach { newPosition ->
+                    queue.add(curr.goTo(newPosition))
+                }
         }
     }
 
-    return maxFlow
+    fun findMaxFlow(partOne: Boolean): Int {
+        if (partOne) return maxFlows.values.maxOrNull()!!
+        else throw NotImplementedError()
+    }
 }
 
 
 fun main() {
     val valves = (DATAPATH / "2022/day16.txt").useLines { lines ->
-        lines.toList().map(::parseLine)
-            .associateBy { it.name }
+        lines.toList()
+            .map(::parseLine)
     }
-    println("Part one: ${maxFlow(valves)}")
+    Solver(valves, 30)
+        .also { println("Part one: ${it.findMaxFlow(true)}")}
+
 }
