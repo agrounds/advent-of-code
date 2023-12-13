ba084a0c3294a4598c35e6b20fb1dc915b53ffd2
Alex Grounds <agrounds90@gmail.com>
2023-12-13 11:13:32 -0600

    Implement 2023 day 13 part two

--- src/main/kotlin/com/groundsfam/advent/y2023/d13/Day13.kt
+++ src/main/kotlin/com/groundsfam/advent/y2023/d13/Day13.kt
@@ -14,26 +14,66 @@ value class Column(val n: Int) : Reflection
 value class Row(val n: Int) : Reflection
 
 
-fun findReflection(pattern: List<String>): Reflection? {
+fun findReflection(pattern: List<List<Char>>, smudged: Boolean): Reflection? {
+
+    // count of differing characters between two lists
+    fun diffs(s1: List<Char>, s2: List<Char>): Int =
+        s1.zip(s2).count { (c1, c2) ->c1 != c2 }
+
     (1 until pattern.size).firstOrNull { y ->
-        (0 until y).all { y1 ->
+        var smudgesRemaining = if (smudged) 1 else 0
+        val rowsMatch = (0 until y).all { y1 ->
             // row number of the reflection of y1 across y
             val y2 = 2 * y - 1 - y1
-            y2 >= pattern.size || pattern[y1] == pattern[y2]
+            // allows us to call diffs() even if y2 is too big
+            val diff = if (y2 >= pattern.size) null else diffs(pattern[y1], pattern[y2])
+            when (diff) {
+                // y2 is too big -- there's nothing to compare this row to
+                null -> true
+                // rows are identical
+                0 -> true
+                // rows are identical up to a smudge
+                1 -> {
+                    smudgesRemaining--
+                    true
+                }
+                // rows are different
+                else -> false
+            }
         }
+        rowsMatch && smudgesRemaining == 0
     }?.also {
         return Row(it)
     }
 
-    (1 until pattern[0].length).firstOrNull { x ->
-        (0 until x).all { x1 ->
+    (1 until pattern[0].size).firstOrNull { x ->
+        var smudgesRemaining = if (smudged) 1 else 0
+        val colsMatch = (0 until x).all { x1 ->
             // column number of the reflection of x1 across x
             val x2 = 2 * x - 1 - x1
-            // compare character by character since we're going down a column
-            pattern.all { line ->
-                x2 >= line.length || line[x1] == line[x2]
+            // allows us to call diffs() even if x2 is too big
+            val diff = x2
+                .takeIf { it < pattern[0].size }
+                ?.let {
+                    val col1 = pattern.map { line -> line[x1] }
+                    val col2 = pattern.map { line -> line[x2] }
+                    diffs(col1, col2)
+                }
+            when (diff) {
+                // x2 is too big -- there's nothing to compare this column to
+                null -> true
+                // columns are identical
+                0 -> true
+                // columns are identical up to a smudge
+                1 -> {
+                    smudgesRemaining--
+                    true
+                }
+                // columns are different
+                else -> false
             }
         }
+        colsMatch && smudgesRemaining == 0
     }?.also {
         return Column(it)
     }
@@ -44,16 +84,16 @@ fun findReflection(pattern: List<String>): Reflection? {
 
 
 fun main() = timed {
-    val patterns: List<List<String>> = (DATAPATH / "2023/day13.txt").useLines { lines ->
-        val parsedPatterns = mutableListOf<List<String>>()
-        var currPattern = mutableListOf<String>()
+    val patterns: List<List<List<Char>>> = (DATAPATH / "2023/day13.txt").useLines { lines ->
+        val parsedPatterns = mutableListOf<List<List<Char>>>()
+        var currPattern = mutableListOf<List<Char>>()
 
         lines.forEach { line ->
             if (line.isBlank()) {
                 parsedPatterns.add(currPattern)
                 currPattern = mutableListOf()
             } else {
-                currPattern.add(line)
+                currPattern.add(line.toList())
             }
         }
         if (currPattern.isNotEmpty()) {
@@ -65,11 +105,20 @@ fun main() = timed {
 
     patterns
         .sumOf { pattern ->
-            when (val r = findReflection(pattern)) {
+            when (val r = findReflection(pattern, smudged = false)) {
                 is Row -> r.n * 100L
                 is Column -> r.n.toLong()
                 null -> throw RuntimeException("No reflection found for pattern $pattern")
             }
         }
         .also { println("Part one: $it") }
+    patterns
+        .sumOf { pattern ->
+            when (val r = findReflection(pattern, smudged = true)) {
+                is Row -> r.n * 100L
+                is Column -> r.n.toLong()
+                null -> throw RuntimeException("No smudged reflection found for pattern $pattern")
+            }
+        }
+        .also { println("Part two: $it") }
 }
