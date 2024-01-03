87ecb9ae462e8ec54b397bc8d043d616d8c7847d
Alex Grounds <agrounds90@gmail.com>
2024-01-03 16:13:56 -0600

    Implement 2023 day 20 part one

--- src/main/kotlin/com/groundsfam/advent/y2023/d20/Day20.kt
+++ src/main/kotlin/com/groundsfam/advent/y2023/d20/Day20.kt
@@ -34,15 +34,48 @@ data object Broadcaster : Module() {
 
 data class ModuleConnection(val module: Module, val destinations: List<String>)
 
+class Solution(private val connections: Map<String, ModuleConnection>) {
+    fun pressButton(): Pair<Int, Int> {
+        data class Pulse(val pulse: Boolean, val from: String, val to: String)
+
+        val pulseQueue = ArrayDeque<Pulse>()
+        pulseQueue.add(Pulse(false, "button", "broadcaster"))
+        var lowPulses = 0
+        var highPulses = 0
+
+        while (pulseQueue.isNotEmpty()) {
+            val (pulse, from, to) = pulseQueue.removeFirst()
+            if (pulse) {
+                highPulses++
+            } else {
+                lowPulses++
+            }
+
+            connections[to]?.let { (module, destinations) ->
+                val nextPulse = module.receive(pulse, from)
+                if (nextPulse != null) {
+                    destinations.forEach { destination ->
+                        pulseQueue.add(Pulse(nextPulse, to, destination))
+                    }
+                }
+            }
+        }
+
+        return Pair(lowPulses, highPulses)
+    }
+}
+
 fun main() = timed {
-    val connections: Map<String, ModuleConnection> = (DATAPATH / "2023/day20.txt").useLines { lines ->
+    val solution = (DATAPATH / "2023/day20.txt").useLines { lines ->
         val inputs = mutableMapOf<String, MutableSet<String>>()
         val outputs = mutableMapOf<String, List<String>>()
         val types = mutableMapOf<String, Char>()
 
         lines.forEach { line ->
             val (module, destinationString) = line.split(" -> ")
-            val moduleName = module.substring(1)
+            val moduleName =
+                if (module[0] == 'b') module // special case -- broadcaster doesnt have a symbol representing its type
+                else module.substring(1)
             types[moduleName] = module[0]
             val destinations = destinationString.split(", ")
 
@@ -67,4 +100,13 @@ fun main() = timed {
             moduleName to ModuleConnection(module, outputs[moduleName]!!)
         }.toMap()
     }
+        .let(::Solution)
+
+    (0 until 1000)
+        .fold(Pair(0L, 0L)) { (lowSum, highSum), _ ->
+            val (low, high) = solution.pressButton()
+            Pair(lowSum + low, highSum + high)
+        }
+        .let { (l, h) -> l * h }
+        .also { println("Part one: $it") }
 }
