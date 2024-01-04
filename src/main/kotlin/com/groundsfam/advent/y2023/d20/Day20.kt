15060d12095717c59e1561b531d8ccd2c2f025aa
Alex Grounds <agrounds90@gmail.com>
2024-01-04 13:40:51 -0600

    Implement 2023 day 20 part two using simplifying assumptions

--- src/main/kotlin/com/groundsfam/advent/y2023/d20/Day20.kt
+++ src/main/kotlin/com/groundsfam/advent/y2023/d20/Day20.kt
@@ -7,9 +7,16 @@ import kotlin.io.path.useLines
 
 
 sealed class Module {
+    /**
+     * Determines what signal (high or low) to send to this module's
+     * destinations, determined from the [pulse] received, and the
+     * module that sent the signal to this module.
+     *
+     * [from] - Name of the module sending the pulse
+     */
     abstract fun receive(pulse: Boolean, from: String): Boolean?
-    abstract fun state(): List<Boolean>
 }
+
 class FlipFlop : Module() {
     private var state: Boolean = false
     override fun receive(pulse: Boolean, from: String): Boolean? =
@@ -20,84 +27,62 @@ class FlipFlop : Module() {
             state
         }
 
-    override fun state(): List<Boolean> = listOf(state)
 }
+
 class Conjunction(inputs: Set<String>) : Module() {
     private val recentPulses = inputs
         .associateWith { false }
         .toMutableMap()
+
     override fun receive(pulse: Boolean, from: String): Boolean {
         recentPulses[from] = pulse
         return recentPulses.any { (_, v) -> !v }
     }
 
-    override fun state(): List<Boolean> = recentPulses.values.toList()
 }
+
 data object Broadcaster : Module() {
     override fun receive(pulse: Boolean, from: String): Boolean = pulse
-    override fun state(): List<Boolean> = emptyList()
 }
 
 data class ModuleConnection(val module: Module, val destinations: List<String>)
-data class RxInputPartition(
-    val modules: Set<Module>,
-    val states: MutableMap<Long, Int> = mutableMapOf(),
-    var period: Int? = null,
-    var repeatFrom: Int? = null,
-    val lowPulses: MutableList<List<Int>>,
-    val highPulses: MutableList<List<Int>>,
-)
+
+// In my input, there is a single conjunction module that connects to rx (call it rxIn),
+// and four conjunction modules that connect to that one. The module graph seems to be
+// partitioned, so that these modules run totally independently of one another.
+// Therefore, my strategy is to identify loops in state for each of these partitions
+// separately, and the time at which these modules send high pulses to rxIn. rxIn sends
+// a low pulse to rx exactly when it receives a high pulse from each of its inputs, and this
+// must happen simultaneously.
+//
+// Further examining my input, I found that high pulses on the rx input modules occur rarely.
+// For each button press, there is at most one high pulse on an rx input, followed by low pulses.
+// In the case that all four rx inputs send high pulses, they all do this before any of them
+// sends low pulses. Thus, the problem reduces to finding the number of button presses in which
+// all of them send high pulses, and their high pulses are guaranteed to produce a low pulse
+// to rx.
+//
+// Examining the input further still, I found that the period by which each partition's state
+// repeats is equal to the number of button presses that that partition first sent a high pulse
+// on its rx input. This makes the modular arithmetic very easy: The puzzle solution is just the
+// least common multiple of the number of button presses to get the first high pulse on each
+// rx input. Furthermore, these numbers are pairwise coprime, so their least common multiple
+// is just their product. The code below uses these simplifying assumptions about the input
+// to simplify the code.
 
 class Solution(private val connections: Map<String, ModuleConnection>) {
-    private val rxInputPartitions: Map<String, RxInputPartition>
+    private val rxInputHighPulse: MutableMap<String, Int?> = mutableMapOf()
     private var buttonPresses: Int = 0
 
     init {
-        // In my input, there is a single conjunction module that connects to rx (call it rxIn),
-        // and four conjunction modules that connect to that one. The module graph seems to be
-        // partitioned, so that these modules run totally independently of one another.
-        // Therefore, our strategy is to identify loops in state for each of these partitions
-        // separately, and the time at which these modules send high pulses to rxIn. rxIn sends
-        // a low pulse to rx exactly when it receives a high pulse from each of its inputs, and this
-        // must happen simultaneously.
-
         fun incoming(moduleName: String): Set<String> =
             connections
                 .filterValues { it.destinations.contains(moduleName) }
                 .keys
 
-        val rxIn = incoming("rx").first()
-        rxInputPartitions = connections
-            .filterValues { it.destinations.contains(rxIn) }
-            .keys
-            .associateWith { moduleName ->
-                val partition = mutableSetOf<Module>()
-                val visited = mutableSetOf<String>()
-                visited.addAll(listOf("broadcaster", rxIn))
-
-                val queue = ArrayDeque<String>()
-                queue.add(moduleName)
-
-                while (queue.isNotEmpty()) {
-                    val name = queue.removeFirst()
-                    visited.add(name)
-                    connections[name]?.module?.also {
-                        partition.add(it)
-                        (incoming(name) + connections[name]!!.destinations).forEach { n ->
-                            if (n !in visited) {
-                                queue.add(n)
-                            }
-                        }
-                    }
-                }
-                RxInputPartition(
-                    partition,
-                    // add empty lists here to represent the fact that no pulses are sent
-                    // before the button has been pressed the first time
-                    lowPulses = mutableListOf(emptyList()),
-                    highPulses = mutableListOf(emptyList())
-                )
-            }
+        incoming("rx").first()
+            .let(::incoming)
+            .forEach { rxInputHighPulse[it] = null }
     }
 
     fun pressButton(): Pair<Int, Int> {
@@ -109,18 +94,17 @@ class Solution(private val connections: Map<String, ModuleConnection>) {
         pulseQueue.add(Pulse(false, "button", "broadcaster"))
         var lowPulses = 0
         var highPulses = 0
-        val rxInputLowPulses = rxInputPartitions.mapValues { mutableListOf<Int>() }
-        val rxInputHighPulses = rxInputPartitions.mapValues { mutableListOf<Int>() }
 
         while (pulseQueue.isNotEmpty()) {
             val (pulse, from, to) = pulseQueue.removeFirst()
             if (pulse) {
                 highPulses++
+                if (from in rxInputHighPulse) {
+                    rxInputHighPulse[from] = buttonPresses
+                }
             } else {
                 lowPulses++
             }
-            val rxInputPulses = if (pulse) rxInputHighPulses else rxInputLowPulses
-            rxInputPulses[from]?.add(lowPulses + highPulses)
 
             connections[to]?.let { (module, destinations) ->
                 val nextPulse = module.receive(pulse, from)
@@ -132,29 +116,17 @@ class Solution(private val connections: Map<String, ModuleConnection>) {
             }
         }
 
-        rxInputPartitions.forEach { (modName, partition) ->
-            val state = partition.modules.flatMap { it.state() }.fold(0L) { s, v ->
-                s * 2 + (if (v) 1 else 0)
-            }
-            val prevButtonPresses = partition.states[state]
-            if (prevButtonPresses == null) {
-                partition.states[state] = buttonPresses
-            } else {
-                partition.repeatFrom = prevButtonPresses
-                partition.period = buttonPresses - prevButtonPresses
-            }
-            partition.lowPulses.add(rxInputLowPulses[modName]!!)
-            partition.highPulses.add(rxInputHighPulses[modName]!!)
-        }
-
         return Pair(lowPulses, highPulses)
     }
 
-    fun seekPartitionPeriods(): List<RxInputPartition> {
-        while (rxInputPartitions.values.any { it.period == null }) {
+    fun findRxLowPulse(): Long {
+        while (rxInputHighPulse.values.any { it == null }) {
             pressButton()
         }
-        return rxInputPartitions.values.toList()
+        return rxInputHighPulse.values
+            .fold(1L) { p, buttonPresses ->
+                p * buttonPresses!!
+            }
     }
 }
 
@@ -182,26 +154,28 @@ fun main() = timed {
             outputs[moduleName] = destinations
         }
 
-        types.map { (moduleName, t) ->
-            val module = when (t) {
-                '%' -> FlipFlop()
-                '&' -> Conjunction(inputs[moduleName] ?: emptySet())
-                'b' -> Broadcaster
-                else -> throw RuntimeException("Invalid module type $t")
-            }
+        types
+            .map { (moduleName, t) ->
+                val module = when (t) {
+                    '%' -> FlipFlop()
+                    '&' -> Conjunction(inputs[moduleName] ?: emptySet())
+                    'b' -> Broadcaster
+                    else -> throw RuntimeException("Invalid module type $t")
+                }
 
-            moduleName to ModuleConnection(module, outputs[moduleName]!!)
-        }.toMap()
+                moduleName to ModuleConnection(module, outputs[moduleName]!!)
+            }
+            .toMap()
+            .let(::Solution)
     }
-        .let(::Solution)
 
     (0 until 1000)
-        .fold(Pair(0L, 0L)) { (lowSum, highSum), i ->
+        .fold(Pair(0L, 0L)) { (lowSum, highSum), _ ->
             val (low, high) = solution.pressButton()
             Pair(lowSum + low, highSum + high)
         }
         .let { (l, h) -> l * h }
         .also { println("Part one: $it") }
-    val res = solution.seekPartitionPeriods()
-    println()
+
+    println("Part two: ${solution.findRxLowPulse()}")
 }
