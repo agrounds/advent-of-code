946109b4d9f2557ad052be0a7a7983137fd09fcc
Alex Grounds <agrounds90@gmail.com>
2024-12-09 11:27:31 -0600

    Implement 2024 day 09 part 2

--- src/main/kotlin/com/groundsfam/advent/y2024/d09/Day09.kt
+++ src/main/kotlin/com/groundsfam/advent/y2024/d09/Day09.kt
@@ -4,8 +4,9 @@ import com.groundsfam.advent.DATAPATH
 import com.groundsfam.advent.timed
 import kotlin.io.path.div
 import kotlin.io.path.readText
+import kotlin.math.min
 
-fun defragmentDisk(diskMap: List<Int>): Long {
+fun defragmentDiskOne(diskMap: List<Int>): Long {
     // for every i, diskMap[2 * i] = number of blocks of file i
 
     // index of diskMap entry
@@ -44,9 +45,62 @@ fun defragmentDisk(diskMap: List<Int>): Long {
     return sum
 }
 
+fun defragmentDiskTwo(diskMap: List<Int>): Long {
+    // mapping from fileId to position of first block on disk
+    val filePositions = IntArray((diskMap.size + 1) / 2)
+    // mapping from length of space to positions of spaces of that length
+    val spaces = (1..9).associateWithTo(mutableMapOf<Int, MutableSet<Int>>()) { mutableSetOf() }
+
+    var pos = 0
+    diskMap.forEachIndexed { i, len ->
+        if (i % 2 == 0) {
+            filePositions[i / 2] = pos
+        } else {
+            // NOTE: This only works because there are no zero-length files in the input,
+            //       hence we do not need to consider concatenating adjacent spaces together
+            if (len > 0) {
+                spaces[len]!!.add(pos)
+            }
+        }
+        pos += len
+    }
+
+    // for each fileId, in descending order, attempt to move it
+    // as far to the left as possible
+    ((diskMap.size - 1) / 2 downTo 0).forEach { fileId ->
+        val fileLen = diskMap[2 * fileId]
+        var minSpaceLen: Int? = null
+        var minSpacePos: Int? = null
+        (fileLen..9).forEach { len ->
+            spaces[len]?.forEach { pos ->
+                if (pos < filePositions[fileId] && minSpacePos?.let { it < pos } != true) {
+                    minSpaceLen = len
+                    minSpacePos = min(pos, minSpacePos ?: pos)
+                }
+            }
+        }
+        if (minSpaceLen != null) {
+            filePositions[fileId] = minSpacePos!!
+            spaces[minSpaceLen]!!.remove(minSpacePos)
+            // length of this space now that we've moved the file here
+            val newSpaceLen = minSpaceLen!! - fileLen
+            if (newSpaceLen > 0) {
+                spaces[newSpaceLen]!!.add(minSpacePos!! + fileLen)
+            }
+        }
+    }
+    return filePositions.indices.sumOf { fileId ->
+        val fileLen = diskMap[2 * fileId]
+        // closed formula for truncated triangle number
+        // a + (a + 1) + (a + 2) + ... + (a + l - 1) = l * (2*a + l - 1) / 2
+        fileId.toLong() * fileLen * (2 * filePositions[fileId] + fileLen - 1) / 2
+    }
+}
+
 fun main() = timed {
     val diskMap = (DATAPATH / "2024/day09.txt").readText().trim().map {
         it.digitToInt()
     }
-    println("Part one: ${defragmentDisk(diskMap)}")
+    println("Part one: ${defragmentDiskOne(diskMap)}")
+    println("Part two: ${defragmentDiskTwo(diskMap)}")
 }
