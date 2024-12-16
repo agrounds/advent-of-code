c961f52588262ce107359169526a38868ee021a5
Alex Grounds <agrounds90@gmail.com>
2024-12-16 12:00:15 -0600

    Implement 2024 day 16 part 2

--- src/main/kotlin/com/groundsfam/advent/y2024/d16/Day16.kt
+++ src/main/kotlin/com/groundsfam/advent/y2024/d16/Day16.kt
@@ -7,36 +7,81 @@ import com.groundsfam.advent.points.*
 import com.groundsfam.advent.timed
 import java.util.PriorityQueue
 import kotlin.io.path.div
+import kotlin.math.min
 
-data class Path(val p: Point, val prevDir: Direction, val pathLen: Long)
+data class State(val p: Point, val dir: Direction)
+data class Path(val state: State, val pathLen: Long)
 
-fun bestPath(grid: Grid<Char>): Long {
+fun bestPath(grid: Grid<Char>): Pair<Long, Int> {
     val start = grid.pointOfFirst { it == 'S' }
     val end = grid.pointOfFirst { it == 'E' }
 
-    val scores = mutableMapOf<Point, Long>()
-
     val queue = PriorityQueue<Path>(compareBy { it.pathLen })
-    val visited = mutableSetOf<Point>()
-    queue.add(Path(start, Direction.RIGHT, 0))
-    while (queue.isNotEmpty()) {
-        val (p, prevDir, pathLen) = queue.poll()
-        scores[p] = pathLen
-        if (p == end) return pathLen
-        if (!visited.add(p)) continue
+    // states get added to visited the first time
+    // a path with that state gets polled from the queue
+    // ensuring we only consider neighbors the first time we see this state
+    // this is guaranteed to be the best path because we're using
+    // a priority queue
+    val visited = mutableSetOf<State>()
+    // map from a state to the length of the best path from the
+    // start state to that state, out of all such paths we know of
+    // so far
+    val bestPath = mutableMapOf<State, Long>()
+    // set of points that are in at least one of the best-known-so-far
+    // paths from the start state to the given state
+    val bestSeats = mutableMapOf<State, Set<Point>>()
+    // signal that helps us end the algorithm
+    // any path with length longer than the best path to end
+    // can be discarded
+    var endPathLen: Long? = null
 
-        listOf(prevDir, prevDir.cw, prevDir.ccw).forEach { dir ->
-            val q = p.go(dir)
-            if (q !in visited && q in grid && grid[q] != '#') {
-                val cost = if (dir == prevDir) 1 else 1001
-                queue.add(Path(q, dir, pathLen + cost))
+    val startState = State(start, Direction.RIGHT)
+    bestPath[startState] = 0
+    bestSeats[startState] = setOf(start)
+    queue.add(Path(startState, 0))
+
+    while (queue.isNotEmpty()) {
+        val (state, pathLen) = queue.poll()
+        val (p, dir) = state
+        if (!visited.add(state)) continue
+        if (pathLen > (endPathLen ?: Long.MAX_VALUE)) continue
+
+        // (state, pathLen) pairs
+        val nextStates =
+            if (p == end) emptyList()
+            else listOf(
+                Pair(State(p, dir.cw), pathLen + 1000),
+                Pair(State(p, dir.ccw), pathLen + 1000),
+            ) +
+                if (grid[p.go(dir)] == '#') emptyList()
+                else listOf(Pair(State(p.go(dir), dir), pathLen + 1))
+
+        nextStates.forEach { (nextState, nextPathLen) ->
+            if (nextState.p == end) {
+                endPathLen = min(nextPathLen, endPathLen ?: Long.MAX_VALUE)
+            }
+            if (nextState !in bestPath || bestPath[nextState]!! > nextPathLen) {
+                bestPath[nextState] = nextPathLen
+                bestSeats[nextState] = bestSeats[state]!! + setOf(nextState.p)
+                queue.add(Path(nextState, nextPathLen))
+            }
+            if (bestPath[nextState] == nextPathLen) {
+                bestSeats[nextState] = bestSeats[nextState]!! + bestSeats[state]!!
+                queue.add(Path(nextState, nextPathLen))
             }
         }
     }
-    throw RuntimeException("No path from start to end")
+
+    val endState = Direction.entries
+        .minBy { bestPath[State(end, it)] ?: Long.MAX_VALUE }
+        .let { State(end, it) }
+
+    return Pair(bestPath[endState]!!, bestSeats[endState]!!.size)
 }
 
 fun main() = timed {
     val grid = (DATAPATH / "2024/day16.txt").readGrid()
-    println("Part one: ${bestPath(grid)}")
+    val (len, numSeats) = bestPath(grid)
+    println("Part one: $len")
+    println("Part two: $numSeats")
 }
